ed5f02f8a177495c11964c0a3ea42632f654adc0
Mariela Pehlivanova <mpehlivanova91@gmail.com>
2022-05-24 13:34:20 +0300

    added

--- src/component/PrintArticle.js
+++ src/component/PrintArticle.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./PrintArticle.css"
 import { validateText, validationUrl } from "../util";
 
 export default function PrintArticle(props) {
--- src/util.js
+++ src/util.js
@@ -54,3 +54,4 @@ export const createServer = () => {
   }
   return arr;
 };
+
